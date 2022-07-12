8a8ff48b74ef13221d18b93554724e37507a1e15
blurdel <5648737+blurdel@users.noreply.github.com>
2022-07-12 19:53:16 -0400

    Updated delete beer to use SPEL syntax for security; matches role-method-assn-delete-beer

--- src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -31,7 +31,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                             .antMatchers(HttpMethod.GET, "/api/v1/beer/**")
                                 .hasAnyRole("ADMIN", "CUSTOMER", "USER")
 
-                            .mvcMatchers(HttpMethod.DELETE, "/api/v1/beer/**").hasRole("ADMIN")
                             .mvcMatchers(HttpMethod.GET, "/api/v1/beerUpc/{upc}")
                                 .hasAnyRole("ADMIN", "CUSTOMER", "USER")
 
