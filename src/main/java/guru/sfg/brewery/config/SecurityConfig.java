30ebfc4f0715551da87999c9d383ecd38bd171cc
blurdel <5648737+blurdel@users.noreply.github.com>
2022-07-16 15:11:57 -0400

    Updated SecurityConfig and Controllers to use authorities like: @PreAuthorize("hasAuthority('beer.read')")

--- src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -26,16 +26,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     authorize
                             .antMatchers("/h2-console/**").permitAll() // TODO: DEV ONLY!!!
 
-                            .antMatchers("/", "/webjars/**", "/login", "/resources/**").permitAll()
+                            .antMatchers("/", "/webjars/**", "/login", "/resources/**").permitAll();
 
-                            .mvcMatchers("/brewery/breweries")
-                                .hasAnyRole("ADMIN", "CUSTOMER")
+//                            .mvcMatchers("/brewery/breweries")
+//                                .hasAnyRole("ADMIN", "CUSTOMER")
 
-                            .mvcMatchers(HttpMethod.GET, "/brewery/api/v1/breweries")
-                                .hasAnyRole("ADMIN", "CUSTOMER")
+//                            .mvcMatchers(HttpMethod.GET, "/brewery/api/v1/breweries")
+//                                .hasAnyRole("ADMIN", "CUSTOMER")
 
-                            .mvcMatchers("/beers/find", "/beers/{beerId}")
-                                .hasAnyRole("ADMIN", "CUSTOMER", "USER");
+//                            .mvcMatchers("/beers/find", "/beers/{beerId}")
+//                                .hasAnyRole("ADMIN", "CUSTOMER", "USER");
 
                 })
                 .authorizeRequests()
