41c22eabfbb80ddb940da319cb2fadbdf6223a01
blurdel <5648737+blurdel@users.noreply.github.com>
2022-07-16 16:16:41 -0400

    Simplified User and JpaUserDetailsService; matches mtc-user-details

--- src/main/java/guru/sfg/brewery/security/JpaUserDetailsService.java
+++ src/main/java/guru/sfg/brewery/security/JpaUserDetailsService.java
@@ -31,30 +31,9 @@ public class JpaUserDetailsService implements UserDetailsService {
 
         log.debug("Getting User info via JPA");
 
-        User user = userRepository.findByUsername(username).orElseThrow(() -> {
+        return userRepository.findByUsername(username).orElseThrow(() -> {
             return new UsernameNotFoundException("Username: " + username + " not found!");
         });
-
-        return new org.springframework.security.core.userdetails.User(
-                user.getUsername(),
-                user.getPassword(),
-                user.isEnabled(),
-                user.isAccountNonExpired(),
-                user.isCredentialsNonExpired(),
-                user.isAccountNonLocked(),
-                convertToSpringAuthorities(user.getAuthorities())
-        );
-    }
-
-    private Collection<? extends GrantedAuthority> convertToSpringAuthorities(Set<Authority> authorities) {
-        if (authorities != null && authorities.size() > 0){
-            return authorities.stream()
-                    .map(Authority::getPermission)
-                    .map(SimpleGrantedAuthority::new)
-                    .collect(Collectors.toSet());
-        } else {
-            return new HashSet<>();
-        }
     }
 
 }
