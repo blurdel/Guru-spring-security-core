0681a1e1df173461dc5149d11102f39947ec26ea
blurdel <5648737+blurdel@users.noreply.github.com>
2022-07-22 17:04:04 -0400

    Added successful login events to be persisted in database

--- src/main/java/guru/sfg/brewery/security/listeners/AuthenticationSuccessListener.java
+++ src/main/java/guru/sfg/brewery/security/listeners/AuthenticationSuccessListener.java
@@ -1,6 +1,9 @@
 package guru.sfg.brewery.security.listeners;
 
+import guru.sfg.brewery.domain.security.LoginSuccess;
 import guru.sfg.brewery.domain.security.User;
+import guru.sfg.brewery.repositories.security.LoginSuccessRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -9,8 +12,12 @@ import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.stereotype.Component;
 
 @Slf4j
+@RequiredArgsConstructor
 @Component
 public class AuthenticationSuccessListener {
+
+    private final LoginSuccessRepository loginSuccessRepository;
+
 
     @EventListener
     public void listen(AuthenticationSuccessEvent event) {
@@ -18,18 +25,29 @@ public class AuthenticationSuccessListener {
         log.debug("User logged in ok ...");
 
         if (event.getSource() instanceof UsernamePasswordAuthenticationToken) {
+            LoginSuccess.LoginSuccessBuilder builder = LoginSuccess.builder();
+
             UsernamePasswordAuthenticationToken token = (UsernamePasswordAuthenticationToken) event.getSource();
 
             if (token.getPrincipal() instanceof User) {
                 User user = (User) token.getPrincipal();
                 log.debug("User logged in: " + user.getUsername());
+
+                // to database
+                builder.user(user);
             }
 
             if (token.getDetails() instanceof WebAuthenticationDetails) {
                 WebAuthenticationDetails details = (WebAuthenticationDetails) token.getDetails();
                 log.debug("Remote Address: " + details.getRemoteAddress());
+
+                // to database
+                builder.sourceIp(details.getRemoteAddress());
             }
 
+            // to database
+            LoginSuccess success = loginSuccessRepository.save(builder.build());
+            log.debug("Login success saved to database, id: " + success.getId());
         }
     }
 }
